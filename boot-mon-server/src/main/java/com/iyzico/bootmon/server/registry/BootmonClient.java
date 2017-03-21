7d861b7db10cd6c63acef4b2d8e881c74a7ad839
Yalçın Yenigün <yalcin.yenigun@iyzico.com>
2017-03-21 19:12:01 +0300

    Spring boot application added. gitignore files moved.

---
+++ boot-mon-server/src/main/java/com/iyzico/bootmon/server/registry/BootmonClient.java
@@ -0,0 +1,43 @@
+package com.iyzico.bootmon.server.registry;
+
+import org.springframework.hateoas.ResourceSupport;
+
+public class BootmonClient extends ResourceSupport {
+
+    private String name;
+    private String ip;
+    private String port;
+    private String healthCheckPath;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getHealthCheckPath() {
+        return healthCheckPath;
+    }
+
+    public void setHealthCheckPath(String healthCheckPath) {
+        this.healthCheckPath = healthCheckPath;
+    }
+}
---
+++ boot-mon-server/src/main/java/com/iyzico/bootmon/server/registry/BootmonClientController.java
@@ -0,0 +1,27 @@
+package com.iyzico.bootmon.server.registry;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping("/api/v1/clients")
+public class BootmonClientController {
+
+    @PostMapping
+    public HttpEntity<BootmonClient> create(@RequestBody BootmonClient bootmonClient) {
+
+        bootmonClient.add(linkTo(methodOn(BootmonClientController.class)
+                .create(bootmonClient))
+                .withSelfRel());
+
+        return new ResponseEntity<BootmonClient>(bootmonClient, HttpStatus.OK);
+    }
+}
